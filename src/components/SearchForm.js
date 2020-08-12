06d91d7b9d28943fd5a7ed6515bc72a79f25fdd6
Mohamad <lahham.m@hotmail.com>
2020-08-12 14:15:08 +0200

    fetch API Data

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,25 @@
 import { Flex, IconButton, Input } from "@chakra-ui/core";
 import React from "react";
 
-export const SearchForm = ({ searchValue, setSearchValue }) => {
+export const SearchForm = ({
+  searchValue,
+  setSearchValue,
+  isLoading,
+  fetchData,
+}) => {
   return (
     <Flex>
       <Input
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
       />
-      <IconButton ml={2} aria-label='Search City' icon='search' />
+      <IconButton
+        ml={2}
+        isLoading={isLoading}
+        aria-label='Search City'
+        icon='search'
+        onClick={() => fetchData()}
+      />
     </Flex>
   );
 };
--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,9 +1,38 @@
-import { Box } from "@chakra-ui/core";
+import { Box, Text, Heading } from "@chakra-ui/core";
 import React, { useState } from "react";
 import { SearchForm } from "./";
+import ky from "ky";
 
 export const WeatherContainer = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const searchParams = new URLSearchParams();
+
+      searchParams.append("q", searchValue);
+      searchParams.append("appid", "24e76e893d88a408ec9ca1575eafdbc0");
+      searchParams.append("units", "metric");
+
+      const result = await ky
+        .get("https://api.openweathermap.org/data/2.5/forecast", {
+          searchParams,
+        })
+        .json();
+
+      setData(result);
+      console.log(result);
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <Box
       p='8'
@@ -14,7 +43,16 @@ export const WeatherContainer = () => {
       mx='4'
       maxWidth='4xl'
     >
-      <SearchForm searchValue={searchValue} setSearchValue={setSearchValue} />
+      <SearchForm
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        isLoading={isLoading}
+        fetchData={fetchData}
+      />
+      {isError && <Text color='red.500'>City not found</Text>}
+      {data?.city?.name && <Heading>{data?.city?.name}</Heading>}
+      {data && data.city && data.city.name && ()}
+
       <Box>days</Box>
       <Box>hourly forcase</Box>
     </Box>
